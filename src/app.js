57bdee13d71358b07014cc73ad29be6331036f8a
Thanh Bui <thanh.bui1995@gmail.com>
2021-04-01 01:47:31 +0300

    initiate project

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './styles/App.css';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
+function App() {
+  return (
+      <Container>
+          <Grid container>
+
+          </Grid>
+      </Container>
+  );
+}
+
+export default App;
---
+++ webpack.dev.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output:{
+        path: path.resolve(__dirname,"./public"),
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname,"./public"),
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env','@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: 'css-loader'
+            },
+        ]
+    }
+}
